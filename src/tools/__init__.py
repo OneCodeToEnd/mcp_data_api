d6faf28af07a2d323bd5978be86f303edacdb4e8
xiedi <18137268181@163.com>
2026-01-30 15:53:44 +0800

    remove overdue doc

--- src/tools/__init__.py
+++ src/tools/__init__.py
@@ -1,10 +1,8 @@
-from .initialize import initialize_session_tool
 from .categories import get_categories_tool
 from .apis import get_apis_by_category_tool, get_api_details_tool
 from .executor import execute_apis_tool
 
 __all__ = [
-    "initialize_session_tool",
     "get_categories_tool",
     "get_apis_by_category_tool",
     "get_api_details_tool",
